52798d0727a21a1471db642960f6e184737378c7
Alessandra Zamora <alessandrazamora@Alessandras-MacBook-Air.local>
2026-02-25 08:18:14 -0500

    Fix base path for GitHub Pages

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/EasyStonks/",
+  base: "/EasyStocks/",
 });
 
 // import { defineConfig } from 'vite'
